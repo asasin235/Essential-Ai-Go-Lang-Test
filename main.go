0efde94d7fc45fdd4a469b3b2062648d1583dfae
Aakif Rashid <aakif@Aakifs-MacBook-Air.local>
2023-09-28 02:40:36 +0530

    Init Commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+*
+Decodes a packet which is in the form of an array of hex charecters into a struct
+in the following format
+*/
+func decodePacket(packet) {
+
+}
+
+func main() {
+
+	fmt.Printf("Decoded Bytes: %v\n", decodedBytes)
+}
